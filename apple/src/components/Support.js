0f5ce873b44111e44867f2dd493055ea2bc0aac0
Kyle Corbin <kylecorbin26@gmail.com>
2019-10-15 18:12:20 -0400

    takign a break to help sam's project

--- apple/src/components/Support.js
+++ apple/src/components/Support.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Route} from "react-router-dom";
 
-function Support(){
+function Support(props){
 
+        
     return(
         <div>
             The Support page
+            <Route path=""></Route>
         </div>
+        
     );
 }
 
